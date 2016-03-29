58e4028ae6a41277a892a7ae821401f4457a890b
Helen Shu <Helen Shu>
2016-03-28 21:23:55 -0700

    Made some more levels

---
+++ src/view/DungeonCrawlStatsBar.java
@@ -0,0 +1,7 @@
+package view;
+
+//this is to show the status bar and skills of the player character; located beneath the playing grid
+public class DungeonCrawlStatsBar {
+
+	//i...guess I'll wait for I to draw those pictures first
+}
